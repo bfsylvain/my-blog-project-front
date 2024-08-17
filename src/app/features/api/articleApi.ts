901102e7a22d0569764c029f4ed0b50d3aec7ad8
bfsylvain <sylvainbonnaure@hotmail.com>
2024-08-17 18:30:59 +0200

    refacto: articleApi query refacto

--- src/app/features/api/articleApi.ts
+++ src/app/features/api/articleApi.ts
@@ -3,23 +3,23 @@ import { ArticleDetail } from "../../../types/ArticleDetail.type.tsx";
 import { CommentToPost } from "../../../types/CommentToPost.tsx";
 import { error } from "console";
 
-// import & export du type à faire si le type se trouve dans la slice
+// import & export du type à faire si le type se trouve dans la slice :
 // import type { Post } from '@/features/posts/postsSlice'
 // export type { Post }
 const backendUrl: string = import.meta.env.VITE_BACKEND_URL as string;
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
-  baseQuery: fetchBaseQuery({ baseUrl: `${backendUrl}/api/` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${backendUrl}/api/articles/` }),
   tagTypes: ["Article"],
   endpoints: (builder) => ({
     getArticles: builder.query<ArticleDetail[], void>({
-      query: () => "articles",
+      query: () => "/",
       providesTags: (result) =>
         result ? result.map(({ _id }) => ({ type: "Article", id: _id })) : [],
     }),
     getArticleById: builder.query<ArticleDetail, string>({
-      query: (id) => `articles/${id}`,
+      query: (id) => `${id}`,
       providesTags: (result, error, id) => [{type: "Article", id}]
     }),
     createNewArticle: builder.mutation<void, FormData>({
@@ -34,7 +34,7 @@ export const articleApi = createApi({
     // void pour le type de l'action elle ne retourne rien et string pour le type du parametre
     deleteArticle: builder.mutation<void, string>({
       query: (id) => ({
-        url: `articles/${id}`,
+        url: `${id}`,
         method: "DELETE",
       }),
       invalidatesTags: (result, error, id) => [{ type: "Article", id }],
@@ -42,7 +42,7 @@ export const articleApi = createApi({
 
     likeArticle: builder.mutation<void, { articleId: string; userId: string }>({
       query: ({ articleId, userId }) => ({
-        url: `/articles/${articleId}/add-liker`,
+        url: `${articleId}/add-liker`,
         method: "PATCH",
         body: { userId },
         credentials: "include",
@@ -56,7 +56,7 @@ export const articleApi = createApi({
       { articleId: string; userId: string }
     >({
       query: ({ articleId, userId }) => ({
-        url: `/articles/${articleId}/remove-liker`,
+        url: `${articleId}/remove-liker`,
         method: "PATCH",
         body: { userId },
         credentials: "include",
@@ -70,7 +70,7 @@ export const articleApi = createApi({
       { articleId: string; commentData: CommentToPost }
     >({
       query: ({ articleId, commentData }) => ({
-        url: `articles/${articleId}/comments`,
+        url: `${articleId}/comments`,
         method: "PATCH",
         body: commentData,
         credentials: "include",
